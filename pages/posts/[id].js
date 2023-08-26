3dc174db3b1680cbfa5909548624d5ae3dd500c0
NIIKURA Shozo <niikura@nyam.co.jp>
2023-08-26 17:53:27 +0900

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import Layout from "../../components/layout"
+import { getAllPostIds, getPostData } from "../../lib/posts"
+import Date from '../../components/date'
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <br />
+      <Date dateString={postData.date} />
+      <br />
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  )
+}
